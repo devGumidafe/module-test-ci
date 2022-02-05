0a71783fa61d11ca6ef9c396d33ecabaf3a4f86c
Gumidafe <devgumidafe@gmail.com>
2022-02-05 12:08:29 +0000

    fixed test

--- src/login.hooks.spec.ts
+++ src/login.hooks.spec.ts
@@ -43,6 +43,6 @@ describe('useLogin specs', () => {
 
     // Assert
     expect(loginStub).toHaveBeenCalled();
-    expect(result.current.user).toEqual(null);
+    expect(result.current.user).toEqual(adminUser);
   });
 });
